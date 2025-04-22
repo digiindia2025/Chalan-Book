f46efc741ed0fbb36ba9dc32fee6479b9cb9ce49
DIGI INDIA SOLUTIONS <digiindiasolutions2023@gmail.com>
2025-04-22 17:11:34 +0530

    url

--- frontend/util/axiosInstance.js
+++ frontend/util/axiosInstance.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:5000",
-  // baseURL: "https://api.ziptech.site",
+  // baseURL: "http://localhost:5000",
+  baseURL: "https://api.ziptech.site",
   headers: {
     "Content-Type": "application/json",
   },
